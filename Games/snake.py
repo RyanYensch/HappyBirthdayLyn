691eb8b24ff9b69d297c777f5026b84be3aa204b
RyanYensch <ry2005@outlook.com>
2024-09-05 03:05:49 +1000

    a lot of linting fixes, ignoring import all, global variable and docustrings

--- Games/snake.py
+++ Games/snake.py
@@ -13,7 +13,7 @@ BACKGROUND_COLOUR = "#FFD1DC"
 XSTART = 0
 YSTART = 0
 
-
+canvas = None
 
 class SnakeGame():
 
@@ -42,24 +42,31 @@ class SnakeGame():
             image = PhotoImage(file= "Images/orangeKitty.png")
             canvas.create_image(x, y, image=image, anchor="nw", tag="food")
             canvas.image = image
+
+    def __init__(self) -> None:
+        self.game_window = None
+        self.score_frame = None
+        self.label = None
+        self.snake = None
+        self.food = None
+        self.score = 0
+        self.direction = 'right'
             
 
-    def gameStart(self):
+    def game_start(self):
         self.game_window = Toplevel()
         self.game_window.title("Snake :)")
         self.game_window["bg"] = HEADER_COLOUR
         self.game_window.resizable(False, False)
 
-        self.score = 0
-        self.direction = 'right'
 
-        self.scoreFrame = Frame(self.game_window, bg=HEADER_COLOUR, width=GAME_WIDTH)
-        self.scoreFrame.pack(fill='x', expand=True)
+        self.score_frame = Frame(self.game_window, bg=HEADER_COLOUR, width=GAME_WIDTH)
+        self.score_frame.pack(fill='x', expand=True)
 
-        self.label = Label(self.scoreFrame, text="Score: {}".format(self.score), font=("consolas", 20, "bold"), bg=HEADER_COLOUR)
+        self.label = Label(self.score_frame, text="Score: {}".format(self.score), font=("consolas", 20, "bold"), bg=HEADER_COLOUR)
         self.label.pack(side="left", pady=10, padx=10)
         
-        back_button = Button(self.scoreFrame, text="Back to Menu", command=self.game_window.destroy)
+        back_button = Button(self.score_frame, text="Back to Menu", command=self.game_window.destroy)
         back_button.pack(side = "right", pady=10, padx=10)
 
         global canvas
@@ -69,33 +76,33 @@ class SnakeGame():
         self.game_window.update()
 
 
-        windowHeight = self.game_window.winfo_height()
-        windowWidth = self.game_window.winfo_width()
-        screenHeight = self.game_window.winfo_screenheight()
-        screenWidth = self.game_window.winfo_screenwidth()
+        window_height = self.game_window.winfo_height()
+        window_width = self.game_window.winfo_width()
+        screen_height = self.game_window.winfo_screenheight()
+        screen_width = self.game_window.winfo_screenwidth()
 
-        x = int((screenWidth/2) - (windowWidth/2))
-        y = int((screenHeight/2) - (windowHeight/2))
+        x = int((screen_width/2) - (window_width/2))
+        y = int((screen_height/2) - (window_height/2))
 
-        self.game_window.geometry(f"{windowWidth}x{windowHeight}+{x}+{y}")
+        self.game_window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-        self.game_window.bind('<Left>', lambda event: self.changeDirection('left'))
-        self.game_window.bind('<Right>', lambda event: self.changeDirection('right'))
-        self.game_window.bind('<Up>', lambda event: self.changeDirection('up'))
-        self.game_window.bind('<Down>', lambda event: self.changeDirection('down'))
-        self.game_window.bind('<a>', lambda event: self.changeDirection('left'))
-        self.game_window.bind('<d>', lambda event: self.changeDirection('right'))
-        self.game_window.bind('<w>', lambda event: self.changeDirection('up'))
-        self.game_window.bind('<s>', lambda event: self.changeDirection('down'))
+        self.game_window.bind('<Left>', lambda event: self.change_direction('left'))
+        self.game_window.bind('<Right>', lambda event: self.change_direction('right'))
+        self.game_window.bind('<Up>', lambda event: self.change_direction('up'))
+        self.game_window.bind('<Down>', lambda event: self.change_direction('down'))
+        self.game_window.bind('<a>', lambda event: self.change_direction('left'))
+        self.game_window.bind('<d>', lambda event: self.change_direction('right'))
+        self.game_window.bind('<w>', lambda event: self.change_direction('up'))
+        self.game_window.bind('<s>', lambda event: self.change_direction('down'))
 
         self.snake = self.Snake()
         self.food = self.Food()
 
-        self.nextTurn(self.snake, self.food)
+        self.next_turn(self.snake, self.food)
 
         return self.game_window
     
-    def nextTurn(self, snake, food):
+    def next_turn(self, snake, food):
         x,y = snake.coordinates[0]
 
         if self.direction == 'up':
@@ -111,35 +118,50 @@ class SnakeGame():
         snake.coordinates.insert(0, (x, y))
         canvas.delete("eyes")
         snake.eyes = []
-        square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOUR, tag="snake")
+        square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE,
+                                         fill=SNAKE_COLOUR, tag="snake")
         snake.squares.insert(0,square)
 
         heart_emoji = "❤︎"
         heart_color = "pink"
 
         if self.direction == 'up':
-            eye1 = canvas.create_text(x + int(SPACE_SIZE / 4), y + int(SPACE_SIZE / 4), text=heart_emoji, font=("Helvetica", 24), fill=heart_color, tag="eyes")
-            eye2 = canvas.create_text(x + int(3 * SPACE_SIZE / 4), y + int(SPACE_SIZE / 4), text=heart_emoji, font=("Helvetica", 24), fill=heart_color, tag="eyes")
+            eye1 = canvas.create_text(x + int(SPACE_SIZE / 4), y + int(SPACE_SIZE / 4),
+                                      text=heart_emoji, font=("Helvetica", 24), fill=heart_color,
+                                      tag="eyes")
+            eye2 = canvas.create_text(x + int(3 * SPACE_SIZE / 4), y + int(SPACE_SIZE / 4),
+                                      text=heart_emoji, font=("Helvetica", 24), fill=heart_color,
+                                      tag="eyes")
             snake.eyes.append([eye1, eye2])
         elif self.direction == 'down':
-            eye1 = canvas.create_text(x + int(SPACE_SIZE / 4), y + int(3 * SPACE_SIZE / 4), text=heart_emoji, font=("Helvetica", 24), fill=heart_color, tag="eyes")
-            eye2 = canvas.create_text(x + int(3 * SPACE_SIZE / 4), y + int(3 * SPACE_SIZE / 4), text=heart_emoji, font=("Helvetica", 24), fill=heart_color, tag="eyes")
+            eye1 = canvas.create_text(x + int(SPACE_SIZE / 4), y + int(3 * SPACE_SIZE / 4),
+                                      text=heart_emoji, font=("Helvetica", 24), fill=heart_color,
+                                      tag="eyes")
+            eye2 = canvas.create_text(x + int(3 * SPACE_SIZE / 4), y + int(3 * SPACE_SIZE / 4),
+                                      text=heart_emoji, font=("Helvetica", 24), fill=heart_color,
+                                      tag="eyes")
             snake.eyes.append([eye1, eye2])
         elif self.direction == 'left':
-            eye1 = canvas.create_text(x + int(SPACE_SIZE / 4), y + int(SPACE_SIZE / 4), text=heart_emoji, font=("Helvetica", 24), fill=heart_color, tag="eyes")
-            eye2 = canvas.create_text(x + int(SPACE_SIZE / 4), y + int(3 * SPACE_SIZE / 4), text=heart_emoji, font=("Helvetica", 24), fill=heart_color, tag="eyes")
+            eye1 = canvas.create_text(x + int(SPACE_SIZE / 4), y + int(SPACE_SIZE / 4),
+                                      text=heart_emoji, font=("Helvetica", 24), fill=heart_color,
+                                      tag="eyes")
+            eye2 = canvas.create_text(x + int(SPACE_SIZE / 4), y + int(3 * SPACE_SIZE / 4),
+                                      text=heart_emoji, font=("Helvetica", 24), fill=heart_color,
+                                      tag="eyes")
             snake.eyes.append([eye1, eye2])
         elif self.direction == 'right':
-            eye1 = canvas.create_text(x + int(3 * SPACE_SIZE / 4), y + int(SPACE_SIZE / 4), text=heart_emoji, font=("Helvetica", 24), fill=heart_color, tag="eyes")
-            eye2 = canvas.create_text(x + int(3 * SPACE_SIZE / 4), y + int(3 * SPACE_SIZE / 4), text=heart_emoji, font=("Helvetica", 24), fill=heart_color, tag="eyes")
+            eye1 = canvas.create_text(x + int(3 * SPACE_SIZE / 4), y + int(SPACE_SIZE / 4),
+                                      text=heart_emoji, font=("Helvetica", 24), fill=heart_color,
+                                      tag="eyes")
+            eye2 = canvas.create_text(x + int(3 * SPACE_SIZE / 4), y + int(3 * SPACE_SIZE / 4),
+                                      text=heart_emoji, font=("Helvetica", 24), fill=heart_color,
+                                      tag="eyes")
             snake.eyes.append([eye1, eye2])
 
-        
 
-        
         if x == food.coordinates[0] and y == food.coordinates[1]:
             self.score += 1
-            self.label.config(text= "Score: {}".format(self.score))
+            self.label.config(text= f"Score: {self.score}")
             canvas.delete("food")
 
             food = self.Food()
@@ -149,35 +171,35 @@ class SnakeGame():
             del snake.squares[-1]
 
 
-        if self.checkCollision(self.snake):
-            self.gameOver()
+        if self.check_collision(self.snake):
+            self.game_over()
         else:
-            self.game_window.after(DELAY, self.nextTurn, snake, food)
+            self.game_window.after(DELAY, self.next_turn, snake, food)
 
-    def changeDirection(self, newDirection):
-        if newDirection == 'left' and self.direction != 'right':
-            self.direction = newDirection
-        elif newDirection == 'right' and self.direction != 'left':
-            self.direction = newDirection
-        elif newDirection == 'up' and self.direction != 'down':
-            self.direction = newDirection
-        elif newDirection == 'down' and self.direction != 'up':
-            self.direction = newDirection
+    def change_direction(self, new_direction) -> None:
+        if new_direction == 'left' and self.direction != 'right':
+            self.direction = new_direction
+        elif new_direction == 'right' and self.direction != 'left':
+            self.direction = new_direction
+        elif new_direction == 'up' and self.direction != 'down':
+            self.direction = new_direction
+        elif new_direction == 'down' and self.direction != 'up':
+            self.direction = new_direction
 
-    def checkCollision(self, snake):
+    def check_collision(self, snake) -> None:
         x, y = snake.coordinates[0]
         if x < 0 or x >= GAME_WIDTH:
             return True
         elif y < 0 or y >= GAME_HEIGHT:
             return True
         
-        for bodyPart in snake.coordinates[1:]:
-            if x == bodyPart[0] and y == bodyPart[1]:
+        for body_part in snake.coordinates[1:]:
+            if x == body_part[0] and y == body_part[1]:
                 return True
             
         return False
 
-    def gameOver(self):
+    def game_over(self) -> None:
         canvas.delete(ALL)
         canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2, font = ('consolas', 70, "bold"), text= "GAME OVER", fill="red", tag="game over")
 
--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from Games.snake import *
 
 BACKGROUND_COLOUR = "#FFD1DC"
 
-def startSnake():
+def start_snake():
     window.withdraw()
     game = SnakeGame()
-    game_window = game.gameStart()
+    game_window = game.game_start()
     window.wait_window(game_window)
     window.deiconify()
 
-def startGame2():
+def start_game2():
     pass
 
 
@@ -18,10 +18,11 @@ window = Tk()
 window.title("FOR THE LOVE OF MY LIFE <3")
 window.config(bg=BACKGROUND_COLOUR)
 
-Title = Label(window, text="Select A Game To Earn Points", bg=BACKGROUND_COLOUR, font=("Arial", 20, "bold"))
+Title = Label(window, text="Select A Game To Earn Points", 
+              bg=BACKGROUND_COLOUR, font=("Arial", 20, "bold"))
 Title.pack(pady=20, side="top")
 
-game_one_button = Button(window, text="Snake", command=startSnake)
+game_one_button = Button(window, text="Snake", command=start_snake)
 game_one_button.pack(pady=10)
 
 
