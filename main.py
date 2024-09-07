6eeedb6b2fe21fc81e12ccb060a2cc0345534a27
RyanYensch <ry2005@outlook.com>
2024-09-07 15:38:48 +1000

    last lint fixxing for now

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ games_beaten = set()
 all_beaten = False
 lines = []
 
+
 def games_done():
     beaten_label.config(text=f"{len(games_beaten)} completed of {TOTAL_GAMES}")
     global all_beaten
     if all_beaten == False and len(games_beaten) == TOTAL_GAMES:
         all_beaten = True
-        beaten_button = Button(window, text="YOU HAVE BEATEN ALL THE GAMES\nCLICK HERE TO GET YOUR REWARD!", font="wingdings", command=get_reward)
+        beaten_button = Button(
+            window, text="YOU HAVE BEATEN ALL THE GAMES\nCLICK HERE TO GET YOUR REWARD!", font="wingdings", command=get_reward)
         beaten_button.pack(pady=20)
 
     return len(games_beaten)
+
 
 def get_reward():
     window.destroy()
@@ -28,17 +31,17 @@ def get_reward():
     win_window.title("FOR THE LOVE OF MY LIFE <3")
     win_window.config(bg=BACKGROUND_COLOUR)
 
-    win_title = Label(win_window, text="OMG YOU DID IT HERES YOUR PRIZE!", 
-                bg=BACKGROUND_COLOUR, font=("Arial", 30, "bold"))
+    win_title = Label(win_window, text="OMG YOU DID IT HERES YOUR PRIZE!",
+                      bg=BACKGROUND_COLOUR, font=("Arial", 30, "bold"))
     win_title.pack(pady=10, side="top")
 
     label1 = Label(win_window, text=lines[0], font=("Arial", 20, "bold"))
     label1.pack()
-    label2 = Label(win_window, text=lines[1], font= ("Arial", 20))
+    label2 = Label(win_window, text=lines[1], font=("Arial", 20))
     label2.pack()
 
-
     win_window.mainloop()
+
 
 def start_snake():
     window.withdraw()
@@ -51,6 +54,7 @@ def start_snake():
 
     games_done()
     window.deiconify()
+
 
 def start_slide():
     window.withdraw()
@@ -81,8 +85,8 @@ def start_mines():
 def read_first_two_lines(file_path):
     try:
         with open(file_path, 'r') as file:
-            lines = [next(file) for _ in range(2)]
-            return lines
+            filelines = [next(file) for _ in range(2)]
+            return filelines
     except FileNotFoundError:
         print(f"The file {file_path} does not exist.")
         return None
@@ -90,22 +94,22 @@ def read_first_two_lines(file_path):
         print(f"The file {file_path} is empty or has fewer than two lines.")
         return None
 
-file_path = 'Prize.txt'
-lines = read_first_two_lines(file_path)
 
+FILE_PATH = 'Prize.txt'
+lines = read_first_two_lines(FILE_PATH)
 
 
 window = Tk()
 window.title("FOR THE LOVE OF MY LIFE <3")
 window.config(bg=BACKGROUND_COLOUR)
 
-Title = Label(window, text="Select A Game To Earn Points", 
+Title = Label(window, text="Select A Game To Earn Points",
               bg=BACKGROUND_COLOUR, font=("Arial", 30, "bold"))
 Title.pack(pady=10, side="top")
 
-beaten_label = Label(window, text = f"0 completed of {TOTAL_GAMES}",
+beaten_label = Label(window, text=f"0 completed of {TOTAL_GAMES}",
                      bg=BACKGROUND_COLOUR, font=("Arial", 20, "bold"))
-beaten_label.pack(pady=(0,20), side="top")
+beaten_label.pack(pady=(0, 20), side="top")
 
 
 game_one_button = Button(window, text="Snake", command=start_snake)
@@ -116,7 +120,6 @@ game_one_button.pack(pady=10)
 
 game_one_button = Button(window, text="Minesweeper", command=start_mines)
 game_one_button.pack(pady=10)
-
 
 
 window.mainloop()
