7566dc74b0e1f6e3ec74dd1c02a88554dd801ac1
RyanYensch <ry2005@outlook.com>
2024-09-07 15:36:46 +1000

    more lint fixing

--- Games/snake.py
+++ Games/snake.py
@@ -17,6 +17,7 @@ REQUIRED_SCORE = 5
 canvas = None
 used_coords = []
 
+
 class SnakeGame():
 
     class Snake():
@@ -33,9 +34,9 @@ class SnakeGame():
             used_coords = self.coordinates
 
             for x, y in self.coordinates:
-                square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOUR, tag="snake")
+                square = canvas.create_rectangle(
+                    x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOUR, tag="snake")
                 self.squares.append(square)
-
 
     class Food():
         def __init__(self):
@@ -44,15 +45,16 @@ class SnakeGame():
             while occupied:
                 occupied = False
                 x = random.randint(0, (GAME_WIDTH/SPACE_SIZE) - 1) * SPACE_SIZE
-                y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE) - 1) * SPACE_SIZE
+                y = random.randint(
+                    0, (GAME_HEIGHT/SPACE_SIZE) - 1) * SPACE_SIZE
 
                 for xcoord, ycoord in used_coords:
                     if x == xcoord and y == ycoord:
                         occupied = True
 
-            self.coordinates = [x,y]
+            self.coordinates = [x, y]
 
-            image = PhotoImage(file= "Images/orangeKitty.png")
+            image = PhotoImage(file="Images/orangeKitty.png")
             canvas.create_image(x, y, image=image, anchor="nw", tag="food")
             canvas.image = image
 
@@ -69,7 +71,6 @@ class SnakeGame():
         if self.score >= REQUIRED_SCORE:
             return True
         return False
-            
 
     def game_start(self):
         self.game_window = Toplevel()
@@ -77,24 +78,25 @@ class SnakeGame():
         self.game_window["bg"] = HEADER_COLOUR
         self.game_window.resizable(False, False)
 
-
-        self.score_frame = Frame(self.game_window, bg=HEADER_COLOUR, width=GAME_WIDTH)
+        self.score_frame = Frame(
+            self.game_window, bg=HEADER_COLOUR, width=GAME_WIDTH)
         self.score_frame.pack(fill='x', expand=True)
 
-        self.label = Label(self.score_frame, text="Score: {}".format(self.score), font=("consolas", 20, "bold"), bg=HEADER_COLOUR)
+        self.label = Label(self.score_frame, text="Score: {}".format(
+            self.score), font=("consolas", 20, "bold"), bg=HEADER_COLOUR)
         self.label.pack(side="left", pady=10, padx=10)
-        
-        back_button = Button(self.score_frame, text="Back to Menu", command=self.game_window.destroy)
-        back_button.pack(side = "right", pady=10, padx=10)
+
+        back_button = Button(
+            self.score_frame, text="Back to Menu", command=self.game_window.destroy)
+        back_button.pack(side="right", pady=10, padx=10)
 
         global canvas
         canvas = Canvas(self.game_window, height=GAME_HEIGHT, width=GAME_WIDTH,
-                        bg = BACKGROUND_COLOUR,
+                        bg=BACKGROUND_COLOUR,
                         highlightbackground="black", highlightthickness=4)
         canvas.pack()
 
         self.game_window.update()
-
 
         window_height = self.game_window.winfo_height()
         window_width = self.game_window.winfo_width()
@@ -106,14 +108,21 @@ class SnakeGame():
 
         self.game_window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-        self.game_window.bind('<Left>', lambda event: self.change_direction('left'))
-        self.game_window.bind('<Right>', lambda event: self.change_direction('right'))
-        self.game_window.bind('<Up>', lambda event: self.change_direction('up'))
-        self.game_window.bind('<Down>', lambda event: self.change_direction('down'))
-        self.game_window.bind('<a>', lambda event: self.change_direction('left'))
-        self.game_window.bind('<d>', lambda event: self.change_direction('right'))
+        self.game_window.bind(
+            '<Left>', lambda event: self.change_direction('left'))
+        self.game_window.bind(
+            '<Right>', lambda event: self.change_direction('right'))
+        self.game_window.bind(
+            '<Up>', lambda event: self.change_direction('up'))
+        self.game_window.bind(
+            '<Down>', lambda event: self.change_direction('down'))
+        self.game_window.bind(
+            '<a>', lambda event: self.change_direction('left'))
+        self.game_window.bind(
+            '<d>', lambda event: self.change_direction('right'))
         self.game_window.bind('<w>', lambda event: self.change_direction('up'))
-        self.game_window.bind('<s>', lambda event: self.change_direction('down'))
+        self.game_window.bind(
+            '<s>', lambda event: self.change_direction('down'))
 
         self.snake = self.Snake()
         self.food = self.Food()
@@ -123,7 +132,7 @@ class SnakeGame():
         return self.game_window
 
     def next_turn(self, snake, food):
-        x,y = snake.coordinates[0]
+        x, y = snake.coordinates[0]
 
         if self.direction == 'up':
             y -= SPACE_SIZE
@@ -134,13 +143,12 @@ class SnakeGame():
         elif self.direction == 'right':
             x += SPACE_SIZE
 
-
         snake.coordinates.insert(0, (x, y))
         canvas.delete("eyes")
         snake.eyes = []
         square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE,
                                          fill=SNAKE_COLOUR, tag="snake")
-        snake.squares.insert(0,square)
+        snake.squares.insert(0, square)
 
         heart_emoji = "❤︎"
         heart_color = "pink"
@@ -178,10 +186,9 @@ class SnakeGame():
                                       tag="eyes")
             snake.eyes.append([eye1, eye2])
 
-
         if x == food.coordinates[0] and y == food.coordinates[1]:
             self.score += 1
-            self.label.config(text= f"Score: {self.score}")
+            self.label.config(text=f"Score: {self.score}")
             canvas.delete("food")
 
             food = self.Food()
@@ -218,15 +225,15 @@ class SnakeGame():
         for body_part in snake.coordinates[1:]:
             if x == body_part[0] and y == body_part[1]:
                 return True
-      
+
         return False
 
     def game_over(self) -> None:
         canvas.delete(ALL)
         canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2,
-                           font = ('consolas', 70, "bold"),
-                           text= "GAME OVER", fill="red", tag="game over")
+                           font=('consolas', 70, "bold"),
+                           text="GAME OVER", fill="red", tag="game over")
         if self.score >= REQUIRED_SCORE:
             canvas.create_text(canvas.winfo_width()/2, 2*canvas.winfo_height()/3,
-                               font = ('consolas',25),
+                               font=('consolas', 25),
                                text="YOU MET THE REQUIRED SCORE\nCONGRATS", fill="red", tag="done")
